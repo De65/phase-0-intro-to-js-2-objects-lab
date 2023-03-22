01d51d34435889c86206946f8d3fbda2f15c5f3c
De65 <kyalodiana6@gmail.com>
2023-03-22 04:13:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const employee ={name:"mars", streetAddress:"harambee"}
-function updateEmployeeWithKeyAndValue(employee, key, value){
-	employee.name = 'Sam'
-	return object.assign({}, employee, {[key]: value})
+
+const employee ={name:"mars",streetAddress:"harambee"}
+
+function  updateEmployeeWithKeyAndValue(employee, key, value){
+	    return Object.assign({}, employee, {[key]: value}); 
 }
 
+
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-	employee[key] = value
-	return employee
-}
-function deleteFromEmployeeByKey(employee, key){
-	const clone = object.assign({}, employee)
-	delete clone[key]
-	return clone
-}
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-	delete employee[key]
-	return employee
-}
+	    employee[key] = value;
+	    return employee;
+	  }
+
+  function deleteFromEmployeeByKey(employee, key){
+	      const clone = Object.assign({}, employee);
+	      delete clone[key];
+	      return clone
+	    }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+	      delete employee[key];
+	      return employee;
+	    }
